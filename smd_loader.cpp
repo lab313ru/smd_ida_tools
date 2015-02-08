cb4b90ae1887aa3090a381b3934a07b936dc1f91
DrMefistO <newinferno@gmail.com>
2015-02-08 20:57:24 +0300

    Fixed some flags. Changes for build configurations.

--- smd_loader.cpp
+++ smd_loader.cpp
@@ -11,6 +11,7 @@
 //
 
 #define NO_OBSOLETE_FUNCS
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <ida.hpp>
 #include <idp.hpp>
@@ -481,23 +482,24 @@ void idaapi load_file(linput_t *li, ushort neflags, const char *fileformatname)
 		//AF_DREFOFF //      0x4000          // Create offset if data xref to seg32 exists
 		//| AF_FINAL //       0x8000          // Final pass of analysis
 		;
-	inf.af2 =
-		AF2_JUMPTBL | //    0x0001          // Locate and create jump tables
-		//AF2_DODATA | //     0x0002          // Coagulate data segs at the final pass
-		//AF2_HFLIRT | //     0x0004          // Automatically hide library functions
-		AF2_STKARG | //     0x0008          // Propagate stack argument information
-		AF2_REGARG | //     0x0010          // Propagate register argument information
-		//AF2_CHKUNI | //     0x0020          // Check for unicode strings
-		//AF2_SIGCMT | //     0x0040          // Append a signature name comment for recognized anonymous library functions
-		AF2_SIGMLT | //     0x0080          // Allow recognition of several copies of the same function
-		AF2_FTAIL | //      0x0100          // Create function tails
-		AF2_DATOFF | //     0x0200          // Automatically convert data to offsets
-		//AF2_ANORET | //     0x0400          // Perform 'no-return' analysis
-		//AF2_VERSP | //      0x0800          // Perform full SP-analysis (ph.verify_sp)
-		//AF2_DOCODE | //     0x1000          // Coagulate code segs at the final pass
-		AF2_TRFUNC | //     0x2000          // Truncate functions upon code deletion
-		//AF2_PURDAT | //     0x4000          // Control flow to data segment is ignored
-		AF2_MEMFUNC; //    0x8000          // Try to guess member function types
+	inf.af2 = 0
+		| AF2_JUMPTBL  //    0x0001          // Locate and create jump tables
+		//| AF2_DODATA  //     0x0002          // Coagulate data segs at the final pass
+		//| AF2_HFLIRT  //     0x0004          // Automatically hide library functions
+		| AF2_STKARG  //     0x0008          // Propagate stack argument information
+		| AF2_REGARG  //     0x0010          // Propagate register argument information
+		//| AF2_CHKUNI  //     0x0020          // Check for unicode strings
+		//| AF2_SIGCMT  //     0x0040          // Append a signature name comment for recognized anonymous library functions
+		| AF2_SIGMLT  //     0x0080          // Allow recognition of several copies of the same function
+		| AF2_FTAIL  //      0x0100          // Create function tails
+		| AF2_DATOFF  //     0x0200          // Automatically convert data to offsets
+		//| AF2_ANORET  //     0x0400          // Perform 'no-return' analysis
+		//| AF2_VERSP  //      0x0800          // Perform full SP-analysis (ph.verify_sp)
+		//| AF2_DOCODE  //     0x1000          // Coagulate code segs at the final pass
+		| AF2_TRFUNC  //     0x2000          // Truncate functions upon code deletion
+		//| AF2_PURDAT  //     0x4000          // Control flow to data segment is ignored
+		//| AF2_MEMFUNC //    0x8000          // Try to guess member function types
+		;
 }
 
 //--------------------------------------------------------------------------
