b7d53673f7fbbae1a278c3dc20fafa04126aceab
Vladimir Kryvian <meffi@lab313.ru>
2015-02-27 18:37:42 +0300

    no message

--- smd_proc/ana.cpp
+++ smd_proc/ana.cpp
@@ -15,6 +15,20 @@ int idaapi ana(void) {
 	cmd.segpref = 0x80;
 
 	uint16 opcode = ua_next_word();
+	uint8 v82 = (opcode >> 9) & 7;
+	uint8 v81_lo = (opcode >> 3) & 7;
+	uint16 v83 = opcode;
+	uint8 v2 = (opcode >> 6) & 7;
+	uint8 v3 = (opcode >> 6) & 7;
+	uint8 v81_hi = opcode & 7;
+
+	switch (opcode >> 12)
+	{
+	case 0x4:
+		if (v2 == 7)
+		{
+		}
+	}
 
 	return 0;
 };
