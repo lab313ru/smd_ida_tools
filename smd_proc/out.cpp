f26b53766123a78d6232db1c60436c1911c54472
DrMefistO <newinferno@gmail.com>
2015-03-07 05:59:35 +0300

    Cleared out and emu code. I have to reimplement this functions from the scratch.

--- smd_proc/out.cpp
+++ smd_proc/out.cpp
@@ -3,81 +3,54 @@
 //
 
 #include "m68k.hpp"
-#include <ints.hpp>
 
-static void print_reg(uint16 reg)
+void idaapi header(void) {
+	MakeLine("My header line");
+}
+
+void idaapi footer(void) {
+	MakeLine("My footer line");
+}
+
+void idaapi segstart(ea_t ea) {
+}
+
+void idaapi segend(ea_t ea) {
+}
+
+static void OutReg(uint16 reg)
 {
 	if (reg == r_a7) reg = r_sp;
 	return out_line(ph.regNames[reg], COLOR_REG);
 }
 
-static void check_print_op1_hex_prefix()
+void idaapi data(ea_t ea)
 {
-	if (cmd.Op1.type == o_idpspec4)
-		if (cmd.Op1.specflag1 & SPEC1_HEX)
-			out_symbol('&');
-}
-
-static bool OutVarName(op_t &op)
-{
-	return (isDefArg(uFlag, op.n) ? 0 : out_name_expr(op, op.addr + 16 * cmd.cs, op.addr));
+	gl_name = 1;
+	intel_data(ea);
 }
 
 bool idaapi outop(op_t &op) {
 	switch (op.type)
 	{
 	case o_void: return 0;
-	case o_reg: print_reg(op.reg); break;
+	case o_reg: OutReg(op.reg); break;
 	case o_phrase:
 	{
 		if ((op.reg & REG_EXT_FLAG) == REG_PRE_DECR) out_symbol('-');
 		if (isDefArg(uFlag, op.n)) OutValue(op);
 
 		out_symbol('(');
-		print_reg(op.reg & REG_MASK + r_a0);
+		OutReg(op.reg & REG_MASK + r_a0);
 		out_symbol(')');
 
 		if ((op.reg & REG_EXT_FLAG) == REG_POST_INCR) out_symbol('+');
-
-		check_print_op1_hex_prefix();
 	}
 	case o_imm:
 	{
-		out_symbol('#');
-
-		switch (op.specflag1)
-		{
-		case 0: OutValue(op); break;
-		case 1: OutValue(op, OOFW_16 | OOFS_NOSIGN); break;
-		case 4:
-		case 5:
-		{
-			OutValue(op, OOFW_8 | OOFS_NOSIGN);
-
-			if (op.specflag1 != 4)
-			{
-				out_symbol(',');
-				print_reg(op.reg);
-			}
-		} break;
-		default: warning("out: %a: bad imm subtype %d", cmd.ip, op.specflag1); break;
-		}
 	}
 	case o_mem:
 	{
-		if (op.specflag1 < 2) out_symbol('(');
-		if (cmd.itype == lea || cmd.itype == pea || !OutVarName(op)) OutValue(op, OOF_ADDR | OOFW_32);
-
-		if (op.specflag1 < 2)
-		{
-			out_symbol(')');
-
-			if (ash.uflag & 1) // TODO: what the flag?
-			{
-				out_symbol('.');
-				out_symbol((op.specflag1 != 0) ? 'l' : 'w');
-			}
-		}
 	}
 	}
 }
