c49b89e117bf9973ebc81a59e110d2ebe2f73c1f
Vladimir Kryvian <meffi@lab313.ru>
2018-10-02 21:02:42 +0300

    Version v1.16

--- smd_loader/smd_loader.cpp
+++ smd_loader/smd_loader.cpp
@@ -5,7 +5,7 @@
 *
 */
 
-#define VERSION "1.15"
+#define VERSION "1.16"
 /*
 *      SEGA MEGA DRIVE/GENESIS ROMs Loader (Modified/Updated HardwareMan's source)
 *      Author: Dr. MefistO [Lab 313] <meffi@lab313.ru>
--- z80_loader/z80_loader.cpp
+++ z80_loader/z80_loader.cpp
@@ -5,7 +5,7 @@
 *
 */
 
-#define VERSION "1.15"
+#define VERSION "1.16"
 /*
 *      SEGA MEGA DRIVE/GENESIS Z80 Drivers Loader
 *      Author: Dr. MefistO [Lab 313] <meffi@lab313.ru>
